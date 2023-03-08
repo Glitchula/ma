ce11f0932222bc157b45225221372f9eb80474d2
Glitchula <65823161+Glitchula@users.noreply.github.com>
2023-03-08 11:02:07 +0100

    ja

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/madapp/generatedhome65widget/GeneratedHOME65Widget.dart';
+import 'package:flutterapp/madapp/generatedhome64widget/GeneratedHOME64Widget.dart';
+import 'package:flutterapp/madapp/generatedhome63widget/GeneratedHOME63Widget.dart';
+import 'package:flutterapp/madapp/generatedhome66widget/GeneratedHOME66Widget.dart';
+import 'package:flutterapp/madapp/generatedhome65widget1/GeneratedHOME65Widget1.dart';
+import 'package:flutterapp/madapp/generatedhome64widget1/GeneratedHOME64Widget1.dart';
+import 'package:flutterapp/madapp/generatedhome63widget1/GeneratedHOME63Widget1.dart';
+import 'package:flutterapp/madapp/generatedhome63widget2/GeneratedHOME63Widget2.dart';
+import 'package:flutterapp/madapp/generatedhome62widget/GeneratedHOME62Widget.dart';
+import 'package:flutterapp/madapp/generatedhome6widget/GeneratedHOME6Widget.dart';
+import 'package:flutterapp/madapp/generatedhome52widget/GeneratedHOME52Widget.dart';
+import 'package:flutterapp/madapp/generatedhome5widget/GeneratedHOME5Widget.dart';
+import 'package:flutterapp/madapp/generatedhome41widget/GeneratedHOME41Widget.dart';
+import 'package:flutterapp/madapp/generatedhome4widget/GeneratedHOME4Widget.dart';
+import 'package:flutterapp/madapp/generatedhome3widget/GeneratedHOME3Widget.dart';
+import 'package:flutterapp/madapp/generatedhome2widget/GeneratedHOME2Widget.dart';
+import 'package:flutterapp/madapp/generatedhome1widget/GeneratedHOME1Widget.dart';
+
+void main() {
+  runApp(MADApp());
+}
+
+class MADApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedHOME1Widget',
+      routes: {
+        '/GeneratedHOME65Widget': (context) => GeneratedHOME65Widget(),
+        '/GeneratedHOME64Widget': (context) => GeneratedHOME64Widget(),
+        '/GeneratedHOME63Widget': (context) => GeneratedHOME63Widget(),
+        '/GeneratedHOME66Widget': (context) => GeneratedHOME66Widget(),
+        '/GeneratedHOME65Widget1': (context) => GeneratedHOME65Widget1(),
+        '/GeneratedHOME64Widget1': (context) => GeneratedHOME64Widget1(),
+        '/GeneratedHOME63Widget1': (context) => GeneratedHOME63Widget1(),
+        '/GeneratedHOME63Widget2': (context) => GeneratedHOME63Widget2(),
+        '/GeneratedHOME62Widget': (context) => GeneratedHOME62Widget(),
+        '/GeneratedHOME6Widget': (context) => GeneratedHOME6Widget(),
+        '/GeneratedHOME52Widget': (context) => GeneratedHOME52Widget(),
+        '/GeneratedHOME5Widget': (context) => GeneratedHOME5Widget(),
+        '/GeneratedHOME41Widget': (context) => GeneratedHOME41Widget(),
+        '/GeneratedHOME4Widget': (context) => GeneratedHOME4Widget(),
+        '/GeneratedHOME3Widget': (context) => GeneratedHOME3Widget(),
+        '/GeneratedHOME2Widget': (context) => GeneratedHOME2Widget(),
+        '/GeneratedHOME1Widget': (context) => GeneratedHOME1Widget(),
+      },
+    );
+  }
+}
